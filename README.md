2f38c600c81bf6ccb4060867aaffe5eaaabbff70
Alexander Weigl <weigl@kit.edu>
2023-12-29 18:58:54 +0100

    create single repository

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# Tools for the KeY ecosystem 
+# tadoc -- Generation of Taclet Documentation 
 
-**Author: Alexander Weigl** under GPLv2+
 
-KeY is a interactive deductive theorem prover for proving the properties in Java Programs. 
-See [key-project.org](https://key-project.org) for more details. 
 
-This mono-repository provides several tools in the KeY ecosystem:
+## TODO/Bugs
 
-1. <citool/> -- A tool for embedding KeY in continous integration workflows. 
-2. <tadoc/> -- Tool for the documentation generation of Taclet (sorts, function, proof rules in KeY)
-3. <miniweb/> --  A simple web-interface for using KeY.
-4. <interactionlog> -- A plugin for KeY to record and re-apply user interaction. 
-
+* pretty printing not working quite right
+  * replacewith could be more formatted
+  * `<sort>::<id>` -- id is not printed
+* documentation formatting, especially trim the indentation
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,158 +1,76 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.sonarqube.gradle.SonarQubePlugin
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.22" apply (false)
-    id("org.jetbrains.dokka") version "1.9.10" apply (false)
-    `maven-publish`
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.dokka") version "1.9.10"
     `java-library`
-    id("com.diffplug.spotless") version "6.23.3" apply false
-    id("org.sonarqube") version "4.4.1.3373"
-
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.github.ben-manes.versions") version "0.50.0"
 }
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
 }
 
-sonar {
-    properties {
-        property("sonar.projectKey", "wadoon_key-tools")
-        property("sonar.organization", "wadoon")
-        property("sonar.host.url", "https://sonarcloud.io")
+application {
+    mainClass = "org.key-project.core.doc.AppKt"
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.ajalt:clikt:2.8.0")
+    implementation("org.jetbrains:annotations:24.1.0")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+    val testImplementation by configurations
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("com.google.truth:truth:1.2.0")
+    testImplementation("org.slf4j:slf4j-simple:2.0.9")
+
+    implementation("org.key-project:key.core:2.12.2")
+    implementation("org.key-project:key.util:2.12.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.10.1")
+    implementation("com.atlassian.commonmark:commonmark:0.17.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-gfm-tables:0.17.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-autolink:0.17.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-gfm-strikethrough:0.17.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-ins:0.17.0")
+    implementation("com.atlassian.commonmark:commonmark-ext-heading-anchor:0.17.0")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
     }
 }
 
+tasks.withType<JavaCompile> {
+    options.release.set(11)
+}
 
-subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "org.jetbrains.dokka")
-    apply(plugin = "maven-publish")
-    apply(plugin = "java-library")
-    //apply(plugin = "com.diffplug.spotless")
-
-    val plugin by configurations.creating
-    configurations {
-        implementation.get().extendsFrom(plugin)
+tasks.withType<Test> {
+    useJUnitPlatform()
+    reports.html.required.set(false)
+    reports.junitXml.required.set(true)
+    testLogging {
+        events("passed", "skipped", "failed")
+        showExceptions = true
     }
+}
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
 
-    repositories {
-        mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
-    }
-
-    dependencies {
-        val implementation by configurations
-
-        val plugin by configurations
-        plugin(platform("org.jetbrains.kotlin:kotlin-bom"))
-        plugin("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        plugin("com.github.ajalt:clikt:2.8.0")
-        plugin("org.jetbrains:annotations:24.1.0")
-        plugin("org.slf4j:slf4j-api:2.0.9")
-
-        //    implementation("org.key-project:key.core")
-
-        val testImplementation by configurations
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-        testImplementation("com.google.truth:truth:1.2.0")
-        testImplementation("org.slf4j:slf4j-simple:2.0.9")
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
-        }
-    }
-
-    tasks.withType<JavaCompile> {
-        options.release.set(11)
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-        reports.html.required.set(false)
-        reports.junitXml.required.set(true)
-        testLogging {
-            events("passed", "skipped", "failed")
-            showExceptions = true
-        }
-    }
-
-    java {
-        withJavadocJar()
-        withSourcesJar()
-    }
-
-    tasks.withType<Javadoc>() {
-        isFailOnError = false
-    }
-
-    /*configure<SpotlessExtension> { // if you are using build.gradle.kts, instead of 'spotless {' use:
-        // configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            // by default the target is every '.kt' and '.kts` file in the java sourcesets
-            //ktfmt()    // has its own section below
-            ktlint().userData(
-                mapOf(
-                    "disabled_rules" to "no-wildcard-imports",
-                    "insert_final_newline" to "true"
-                )
-            )
-            // has its own section below
-            //diktat()   // has its own section below
-            //prettier() // has its own section below
-            licenseHeaderFile("$rootDir/gradle/license_header")  // '/* (C)$YEAR */' // or licenseHeaderFile
-        }
-        kotlinGradle {
-            //target("*.gradle.kts") // default target for kotlinGradle
-            //ktlint() // or ktfmt() or prettier()
-        }
-    }*/
-
-
-
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-                pom {
-                    url.set("http://github.com/wadoon/key-tools")
-                    licenses {
-                        license {
-                            name.set("GPLv2+")
-                            url.set("https://www.gnu.org/licenses/old-licenses/gpl-2.0.html")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("weigl")
-                            name.set("Alexander Weigl ")
-                            email.set("weigl@kit.edu")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/wadoon/key-tools.git")
-                        developerConnection.set("scm:git:git@github.com:wadoon/key-tools.git")
-                        url.set("http://github.com/wadoon/key-tools")
-                    }
-                }
-            }
-            repositories {
-                maven {
-                    name = "GitHubPackages"
-                    url = uri("https://maven.pkg.github.com/wadoon/key-tools")
-                    credentials {
-                        username = System.getenv("GITHUB_ACTOR")
-                        password = System.getenv("GITHUB_TOKEN")
-                    }
-                }
-            }
-        }
-    }
+tasks.withType<Javadoc>() {
+    isFailOnError = false
 }
